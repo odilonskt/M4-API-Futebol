fbb7bdea437412c9874e77870ee1a5738f2c625c
odilonskt <odilon123c@gmail.com>
2025-04-11 17:11:17 -0300

    adicionando mais valores no db

--- src/database/db.js
+++ src/database/db.js
@@ -2,23 +2,55 @@
   
   // Times
   export const times = [
-    { id: 1, nome: 'Flamengo' },
-    { id: 2, nome: 'Palmeiras' },
-    { id: 3, nome: 'Corinthians' },
-    { id: 4, nome: 'Grêmio' },
+    { id: 1, nome: 'Flamengo', pais: 'Brasil' },
+    { id: 2, nome: 'Palmeiras', pais: 'Brasil' },
+    { id: 3, nome: 'Corinthians', pais: 'Brasil' },
+    { id: 4, nome: 'Grêmio', pais: 'Brasil' },
     { id: 5, nome: 'Kashima Antlers', pais: 'Japão' },
-    { id: 6, nome: 'Real Madrid', pais: 'Espanha' }
+    { id: 6, nome: 'Real Madrid', pais: 'Espanha' },
+    { id: 7, nome: 'Chelsea', pais: 'Inglaterra' },
+    { id: 8, nome: 'Bayern de Munique', pais: 'Alemanha' },
+    { id: 9, nome: 'PSG', pais: 'França' },
+    { id: 10, nome: 'Al-Nassr', pais: 'Arábia Saudita' },
+    { id: 11, nome: 'Ajax', pais: 'Holanda' },
+    { id: 12, nome: 'Benfica', pais: 'Portugal' },
+    { id: 13, nome: 'Borussia Dortmund', pais: 'Alemanha' },
+    { id: 14, nome: 'Club Brugge', pais: 'Bélgica' },
+    { id: 15, nome: 'Fenerbahçe', pais: 'Turquia' },
+    { id: 16, nome: 'Wydad Casablanca', pais: 'Marrocos' }
   ];
   
   // Jogadores
    export const jogadores = [
-    { id: 1, nome: 'Pedro', idade: 26, timeId: 1 },
-    { id: 2, nome: 'Gustavo Gómez', idade: 30, timeId: 2 },
-    { id: 3, nome: 'Cássio', idade: 35, timeId: 3 },
-    { id: 4, nome: 'Luan', idade: 27, timeId: 4 },
-    { id: 5, nome: 'Gabigol', idade: 28, timeId: 1 },
-    { id: 6, nome: 'Shinji Okazaki', idade: 37, timeId: 5 }, // Jogador japonês
-    { id: 7, nome: 'Takefusa Kubo', idade: 23, timeId: 5 },  // Jogador japonês
-    { id: 8, nome: 'Luka Modrić', idade: 38, timeId: 6 },    // Jogador europeu
-    { id: 9, nome: 'Karim Benzema', idade: 35, timeId: 6 }
+    { id: 1, nome: 'Pedro', idade: 26, timeId: 1, nacionalidade: 'Brasileiro' },
+  { id: 2, nome: 'Gustavo Gómez', idade: 30, timeId: 2, nacionalidade: 'Paraguaio' },
+  { id: 3, nome: 'Cássio', idade: 35, timeId: 3, nacionalidade: 'Brasileiro' },
+  { id: 4, nome: 'Luan', idade: 27, timeId: 4, nacionalidade: 'Brasileiro' },
+  { id: 5, nome: 'Gabigol', idade: 28, timeId: 1, nacionalidade: 'Brasileiro' },
+  { id: 6, nome: 'Shinji Okazaki', idade: 37, timeId: 5, nacionalidade: 'Japonês' },
+  { id: 7, nome: 'Takefusa Kubo', idade: 23, timeId: 5, nacionalidade: 'Japonês' },
+  { id: 8, nome: 'Luka Modrić', idade: 38, timeId: 6, nacionalidade: 'Croata' },
+  { id: 9, nome: 'Karim Benzema', idade: 35, timeId: 6, nacionalidade: 'Francês' },
+  { id: 10, nome: 'Mason Mount', idade: 25, timeId: 7, nacionalidade: 'Inglês' },
+  { id: 11, nome: 'Kai Havertz', idade: 24, timeId: 8, nacionalidade: 'Alemão' },
+  { id: 12, nome: 'Kylian Mbappé', idade: 26, timeId: 9, nacionalidade: 'Francês' },
+  { id: 13, nome: 'Achraf Hakimi', idade: 25, timeId: 9, nacionalidade: 'Marroquino' },
+  { id: 14, nome: 'Cristiano Ronaldo', idade: 39, timeId: 10, nacionalidade: 'Português' },
+  { id: 15, nome: 'Sadio Mané', idade: 32, timeId: 10, nacionalidade: 'Senegalês' },
+  { id: 16, nome: 'Hakim Ziyech', idade: 30, timeId: 16, nacionalidade: 'Marroquino' },
+  { id: 17, nome: 'Edson Álvarez', idade: 26, timeId: 11, nacionalidade: 'Mexicano' },
+  { id: 18, nome: 'Steven Berghuis', idade: 32, timeId: 11, nacionalidade: 'Holandês' },
+  { id: 19, nome: 'João Mário', idade: 31, timeId: 12, nacionalidade: 'Português' },
+  { id: 20, nome: 'Ángel Di María', idade: 36, timeId: 12, nacionalidade: 'Argentino' },
+  { id: 21, nome: 'Marco Reus', idade: 34, timeId: 13, nacionalidade: 'Alemão' },
+  { id: 22, nome: 'Karim Adeyemi', idade: 23, timeId: 13, nacionalidade: 'Alemão' },
+  { id: 23, nome: 'Simon Mignolet', idade: 36, timeId: 14, nacionalidade: 'Belga' },
+  { id: 24, nome: 'Andreas Skov Olsen', idade: 25, timeId: 14, nacionalidade: 'Dinamarquês' },
+  { id: 25, nome: 'Edin Džeko', idade: 38, timeId: 15, nacionalidade: 'Bósnio' },
+  { id: 26, nome: 'Michy Batshuayi', idade: 30, timeId: 15, nacionalidade: 'Belga' },
+  { id: 27, nome: 'Yahia Attiyat Allah', idade: 29, timeId: 16, nacionalidade: 'Marroquino' },
+  { id: 28, nome: 'Soufiane Rahimi', idade: 28, timeId: 16, nacionalidade: 'Marroquino' },
+  { id: 29, nome: 'Dusan Tadic', idade: 35, timeId: 11, nacionalidade: 'Sérvio' },
+  { id: 30, nome: 'Enzo Fernández', idade: 23, timeId: 7, nacionalidade: 'Argentino' }
   ];
+
