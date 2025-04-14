d4c6b062d96b976e7ec4cd479a21c454cb157318
odilonskt <odilon123c@gmail.com>
2025-04-14 11:08:21 -0300

    adicionando comentario nas rotas

--- src/routes/time.routes.js
+++ src/routes/time.routes.js
@@ -3,12 +3,17 @@ import TimeController from "../controller/time.controller.js";
 
 const timeRouter = Router()
 
+
+//*pegar todos os times 
 timeRouter.get('/',(req,res)=> TimeController.getAllTime(req,res));
 
+//*pegar time pelo id 
 timeRouter.get('/:id', (req,res)=> TimeController.squardSelectTimeID(req,res));
 
+//*pegar todos os jogadores que fazem parte do time 
 timeRouter.get('/:id/jogadores',(req, res) => TimeController.getJogadoresDoTime(req, res))
 
+//*pegar o pais e mostra todos os time que fazem parte da quele Estado 
 timeRouter.get('/pais/:pais', (req, res) => TimeController.getPais(req, res));
 
 export default  timeRouter;
