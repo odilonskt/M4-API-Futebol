d54f4993a7ce34e0f517ccde623073f42a82cc20
odilonskt <odilon123c@gmail.com>
2025-04-14 11:08:55 -0300

    adicionando comentario na rota jogadores

--- src/routes/jogadores.routes.js
+++ src/routes/jogadores.routes.js
@@ -5,10 +5,14 @@ import JogadoresController from "../controller/jogardores.controller.js";
 const jogadorRouter = Router();
 
 // rota principal: GET /jogadores
+//*mostra todos os jogadores
 jogadorRouter.get('/', (req, res) => JogadoresController.getAllJogador(req, res));
 
+//*pegar o jogador em especifico pelo id 
 jogadorRouter.get('/:id',(req,res)=> JogadoresController.squardSelectJogadoresID(req,res))
 
+
+//*  pegar os jogadores pela sua nacionalidade 
 jogadorRouter.get('/nacionalidade/:nacionalidade',(req,res)=> JogadoresController.getNacionalidade(req,res))
 
 export default jogadorRouter;
