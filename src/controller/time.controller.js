c09389236e77e4a648c0ea5b05f806d6faa90eab
odilonskt <odilon123c@gmail.com>
2025-04-22 11:25:11 -0300

    adicionando função de media, minimo e  maximo

--- src/controller/time.controller.js
+++ src/controller/time.controller.js
@@ -42,6 +42,33 @@ class TimeController{
         // Retorna os times encontrados
         return res.json(timesDoPais);
     }
+
+    getMaxByGol(req, res){
+        const maxGol = Math.max(...times.map(time => time.gols));
+
+        const timesComMaxGol = times.filter(time => time.gols === maxGol)
+
+        return res.json({ maxGol, times: timesComMaxGol })
+    }
+
+    getMinByGol(req,res){
+        const minGol = Math.min(...times.map(time => time.gols));
+
+        const timesComMinGol = times.filter(time=> time.gols === minGol )
+
+        return res.json({minGol,times:timesComMinGol})
+    }
+
+    getMediaByGols(req,res){
+        const totalGols = times.reduce((soma,time)=> soma + time.gols, 0);
+
+        const mediaGols = totalGols / times.length;
+
+        // Filtra os times que possuem o número de gols igual à média
+        const timesNaMedia = times.filter(time => time.gols === Math.round(mediaGols))
+
+        return res.json({mediaGols,timesNaMedia});
+    }
 }
 
 
